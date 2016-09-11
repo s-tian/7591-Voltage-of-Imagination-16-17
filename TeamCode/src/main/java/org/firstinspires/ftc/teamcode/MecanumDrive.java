6b42131bb749cbb1459f28649995544529e9b66c
Stephen Tian <stephen.st123@gmail.com>
2016-09-11 14:46:18 -0700

    Add basic mecanum drive opmode

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -0,0 +1,46 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+
+/**
+ * Created by Stephen on 9/11/2016.
+ */
+
+@TeleOp(name = "Mecanum Drive", group = "Drive")
+
+public class MecanumDrive extends LinearOpMode {
+
+    DcMotor frontLeft, frontRight, backLeft, backRight;
+
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+
+        frontLeft = hardwareMap.dcMotor.get("frontLeft");
+        frontRight = hardwareMap.dcMotor.get("frontRight");
+        backLeft = hardwareMap.dcMotor.get("backLeft");
+        backRight = hardwareMap.dcMotor.get("backRight");
+        backRight.setDirection(DcMotorSimple.Direction.REVERSE);
+        frontRight.setDirection(DcMotorSimple.Direction.REVERSE);
+
+
+
+        waitForStart();
+        while(opModeIsActive()) {
+
+            double joy1Y = -gamepad1.left_stick_y;
+            double joy1X = gamepad1.left_stick_x;
+            double joy2X = gamepad1.right_stick_x;
+
+            frontLeft.setPower(Math.min(1, joy1Y + joy2X + joy1X));
+            backLeft.setPower(Math.min(1, joy1Y + joy2X - joy1X));
+            frontRight.setPower(Math.min(1, joy1Y - joy2X - joy1X));
+            backRight.setPower(Math.min(1, joy1Y - joy2X + joy1X));
+        }
+
+    }
+}
