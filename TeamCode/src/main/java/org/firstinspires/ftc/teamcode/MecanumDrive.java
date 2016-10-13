fe2789897950b816cee71196bc1a5673294e7c79
bunnycide <bunnycide@Andys-MacBook-Pro.local>
2016-10-12 20:13:22 -0700

    Added shooter code to mecum drive for testing

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MecanumDrive.java
@@ -6,6 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
  * Created by Stephen on 9/11/2016.
@@ -15,16 +16,28 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 public class MecanumDrive extends LinearOpMode {
 
-    DcMotor frontLeft, frontRight, backLeft, backRight;
+    DcMotor frontLeft, frontRight, backLeft, backRight, motorA, motorB, motorC;
 
     @Override
     public void runOpMode() throws InterruptedException {
+
+        boolean increased = false ,decreased = false;
+        boolean cIncreased = false, cDecreased = false;
 
         frontLeft = hardwareMap.dcMotor.get("frontLeft");
         frontRight = hardwareMap.dcMotor.get("frontRight");
         backLeft = hardwareMap.dcMotor.get("backLeft");
         backRight = hardwareMap.dcMotor.get("backRight");
+        motorA = hardwareMap.dcMotor.get("motorA");
+        motorB = hardwareMap.dcMotor.get("motorB");
+        motorC = hardwareMap.dcMotor.get("motorC");
 
+        motorA.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorB.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        motorA.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motorB.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.FLOAT);
+        motorA.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorC.setDirection(DcMotorSimple.Direction.REVERSE);
         frontLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         frontRight.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
         backLeft.setMode(DcMotor.RunMode.RUN_WITHOUT_ENCODER);
@@ -35,6 +48,9 @@ public class MecanumDrive extends LinearOpMode {
 
         waitForStart();
         while(opModeIsActive()) {
+            telemetry.addData("Flywheel", motorA.getPower());
+            telemetry.addData("Conveyor", motorC.getPower());
+
             double joy1Y = -gamepad1.left_stick_y;
             joy1Y = Math.abs(joy1Y) > 0.15 ? joy1Y*3/4: 0;
             double joy1X = gamepad1.left_stick_x;
@@ -45,6 +61,39 @@ public class MecanumDrive extends LinearOpMode {
             backLeft.setPower(Math.max(-1, Math.min(1, joy1Y + joy2X - joy1X)));
             frontRight.setPower(Math.max(-1, Math.min(1, joy1Y - joy2X - joy1X)));
             backRight.setPower(Math.max(-1, Math.min(1, joy1Y - joy2X + joy1X)));
+
+            if (gamepad1.a && motorA.getPower() <= .9 && !increased) {
+                motorA.setPower(motorA.getPower() + .1);
+                motorB.setPower(motorA.getPower());
+                increased = true;
+            }
+            if (gamepad1.b && motorA.getPower() >= .1 && !decreased) {
+                motorA.setPower(motorA.getPower() - .1);
+                motorB.setPower(motorA.getPower());
+                decreased = true;
+            }
+            if (gamepad1.x && motorC.getPower() <= .9 && !cIncreased){
+                motorC.setPower(motorC.getPower() + .1);
+                cIncreased = true;
+            }
+            if (gamepad1.y && motorC.getPower() >= .1 && !cDecreased){
+                motorC.setPower(motorC.getPower()- .1);
+                cDecreased = true;
+            }
+            if (!gamepad1.a){
+                increased = false;
+            }
+            if (!gamepad1.b) {
+                decreased = false;
+            }
+            if (!gamepad1.x){
+                cIncreased = false;
+            }
+            if (!gamepad1.y){
+                cDecreased = false;
+            }
+
+            telemetry.update();
 
         }
     }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/ShooterTest.java
@@ -5,7 +5,6 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
-
 import org.firstinspires.ftc.robotcore.external.Telemetry;
 
 /**
