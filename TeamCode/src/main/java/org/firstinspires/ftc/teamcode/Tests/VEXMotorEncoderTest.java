3fd0f275fcbfe9f8dfbdc90a94ced1117695c4e0
Stephen Tian <stephen.st123@gmail.com>
2017-01-22 19:44:57 -0800

    Add VEX Motor encoder test

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Tests/VEXMotorEncoderTest.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode.Tests;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.CRServo;
+
+import org.firstinspires.ftc.teamcode.robotutil.VEX393Encoder;
+
+/**
+ * Created by Stephen on 1/22/2017.
+ */
+
+public class VEXMotorEncoderTest extends LinearOpMode {
+    CRServo vexMotor;
+
+    @Override
+    public void runOpMode() {
+        vexMotor = hardwareMap.crservo.get("sweeper1");
+        VEX393Encoder encoder = new VEX393Encoder(hardwareMap, "vexencoder");
+        waitForStart();
+        while(opModeIsActive()) {
+            vexMotor.setPower(-gamepad1.left_stick_y);
+            telemetry.addData("Encoder Ticks ", encoder.readRotation());
+            telemetry.addData("Motor speed ", encoder.getSignedVelocity());
+        }
+
+    }
+}
